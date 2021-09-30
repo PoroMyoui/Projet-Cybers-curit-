19b7a9d6f6c2bcea9e46ab3dad93d25fcc07a9c6
Reednar <lepieloic94@gmail.com>
2021-09-30 16:45:51 +0200

    Loic

--- login.php
+++ login.php
@@ -1,6 +1,20 @@
 <?php include 'vues/header.php' ?>
 
+<div class="container">
+
+    <form action="" method="POST" id="form_login">
+
+    <div class="form-group">
+      <label for="inputUsername" class="form-label mt-4">Username</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username">
+    </div>
+</div>
+
+
+</form>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
 </html>
---
+++ vues/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://bootswatch.com/5/litera/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Projet cybersecurity</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor03">
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+            <a class="nav-link" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="log.php">Comptes</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Connexion</a>
+            </li>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="inscription.php">Inscription</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+</body>
